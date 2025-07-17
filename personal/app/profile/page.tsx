ffda74d0304e311f43055a68b61dee1aec2ac044
EbadullahSiddiqi <official.ebadullahsiddiqi@gmail.com>
2025-07-18 03:50:10 +0500

    Added my brand colors and now I'm creating the Navbar. My image and name are done, will complete it tomorrow and create a good home page

---
+++ personal/app/profile/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>Ebadullah Saddique's Profile</div>
+  )
+}
+
+export default page
---
+++ personal/components/Navbar.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+function Navbar() {
+  const router = useRouter();
+
+  return (
+    <div>
+      <nav>
+        <div className="flex justify-between items-center p-4">
+          {/* Name and Pic div*/}
+          <div className="flex gap-3 items-center justify-between">
+            <div className="hover:scale-110 transition-transform duration-300">
+              <Image
+                className="rounded-full border-2 border-[#ffeca0] cursor-pointer"
+                src="/me2.png"
+                alt="my image"
+                width={70}
+                height={70}
+                onClick={() => {
+                  router.push("/profile");
+                }}
+              />
+            </div>
+
+            <h1 className="text-3xl font-bold">Ebad</h1>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
