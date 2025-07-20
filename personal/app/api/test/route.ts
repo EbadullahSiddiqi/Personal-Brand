4bc78d4710e2887662bf4fc3f7d0edf8c5a07939
EbadullahSiddiqi <official.ebadullahsiddiqi@gmail.com>
2025-07-21 03:44:02 +0500

    Removed the /api/test route

--- personal/app/api/test/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server"
-
-export async function GET() {
-    console.log(process.env.DATABASE_URL)
-    return NextResponse.json({
-        neon: process.env.DATABASE_URL
-    })
-}
